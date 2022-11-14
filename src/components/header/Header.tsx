6bd1b0ea9043a83faed08c2f53aaa71d494bc87e
ilyas-g <ilyas15069i@gmail.com>
2022-11-14 13:27:34 +0100

    Ajout d'un toggle sur le menu burger

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
+import BurgerMenu from '../burgerMenu/BurgerMenu';
 import "./style.css"
 
 export default function Header() {
+
+  const [burger, setBurger] = useState<boolean>(false)
+
   return (
     <header>
         <img src="https://yuzugaming.com/site/templates/assets/ope/pfl/pfl-logo-crop.png" width={115}/>
         <div className='header-navigation'>
-          <span className='icon-menu'></span>
+          <span className='icon-menu' onClick={() => {setBurger(!burger)}}></span>
+          {burger === true ? <BurgerMenu /> : ''}
+
           <nav className='main-navigation'>
             <ul role="list">
               <li role="listitem" className="onglets"><Link to="/">Accueil</Link></li>
