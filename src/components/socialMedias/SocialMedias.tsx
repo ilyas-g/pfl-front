32ad511f47dfbe84267eea6337e2104a53d0080a
ilyas-g <ilyas15069i@gmail.com>
2022-11-27 00:44:06 +0100

    Ajout d'une icone pour la vidéo de la pfl 2

--- src/components/socialMedias/SocialMedias.tsx
+++ src/components/socialMedias/SocialMedias.tsx
@@ -4,6 +4,11 @@ import "./style.css"
 export default function SocialMedias() {
   return (
     <aside className="socials-aside socials-aside socials-aside_ready">
+        <a href={import.meta.env.VITE_DISCORD} target="_blank" title="TrailerPFL2" aria-label="TrailerPFL2" className="pfl-trailer tooltip">
+          <span className='icon-video-camera1'>
+          </span>
+            <span className="tooltiptext">Trailer de la PFL#2</span>
+        </a>
         <a href={import.meta.env.VITE_DISCORD} target="_blank" title="Discord" aria-label="Discord"><span className='icon-discord'></span></a>
         <a href={import.meta.env.VITE_TWITCH} target="_blank" title="Twitch" aria-label="Twitch"><span className='icon-twitch'></span></a>
         <a href={import.meta.env.VITE_TWITTER} target="_blank" title="Twitter" aria-label="Twitter"><span className='icon-twitter'></span></a>
