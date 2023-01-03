4631ecec6018fc283e5cc4363c8fb1c0c8a42bc1
ilyas-g <ilyas15069i@gmail.com>
2023-01-03 18:56:46 +0100

    Ajout d'un bouton NSxC dans les réseaux sociaux et modifications du bouton Yuzu Gaming

--- src/components/socialMedias/SocialMedias.tsx
+++ src/components/socialMedias/SocialMedias.tsx
@@ -11,6 +11,7 @@ export default function SocialMedias({func}) {
         <a href={import.meta.env.VITE_TWITCH} target="_blank" title="Twitch's FlashNo47" aria-label="Twitch" className='icon-twitch'></a>
         <a href={import.meta.env.VITE_TWITTER} target="_blank" title="Twitter Yuzu" aria-label="Twitter" className='icon-twitter'></a>
         <a href={import.meta.env.VITE_WEBSITE} target="_blank" title="Yuzu Gaming website" aria-label="Twitter" className="yuzu-social icon-yuzu"></a>
+        <a href={import.meta.env.VITE_NSXC} target="_blank" title="NSxC Twitter" aria-label="NSxC" className="nsxc-social icon-nsxc"></a>
     </aside>
   )
 }
