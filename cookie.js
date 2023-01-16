390d43e9ab672d1d58b91040e24a760cc985ec56
ilyas-g <ilyas15069i@gmail.com>
2023-01-16 14:39:18 +0100

    Ajout de cookie

---
+++ cookie.js
@@ -0,0 +1,12 @@
+window.axeptioSettings = {
+    clientId: "63c548b112fbd07ae713ba2d",
+    cookiesVersion: "parisiennefightingligue-fr",
+};
+
+(function (d, s) {
+    var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+    // e.async = true; e.src = "//static.axept.io/sdk.js";
+    e.async = true; e.src = "//static.axept.io/sdk-slim.js";
+
+    t.parentNode.insertBefore(e, t);
+})(document, "script");
