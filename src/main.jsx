65bff5ca7cc17bbcbc40da76f989f795d9a86a87
ilyas-g <ilyas15069i@gmail.com>
2023-01-16 14:50:38 +0100

    Ajout du cookie dans le main file

--- src/main.jsx
+++ src/main.jsx
@@ -33,6 +33,20 @@ const client = new ApolloClient({
   cache: new InMemoryCache()
 });
 
+window.axeptioSettings = {
+  clientId: "63c548b112fbd07ae713ba2d",
+  cookiesVersion: "parisiennefightingligue-fr",
+};
+
+(function (d, s) {
+  var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+  // e.async = true; e.src = "//static.axept.io/sdk.js";
+  e.async = true; e.src = "//static.axept.io/sdk-slim.js";
+
+  t.parentNode.insertBefore(e, t);
+})(document, "script");
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <React.StrictMode>
