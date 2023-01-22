dfd072871a25f99feb993a06342c09b574416e86
ilyas-g <ilyas15069i@gmail.com>
2023-01-23 00:05:10 +0100

    Ajout du bouton d'inscription

--- src/App.tsx
+++ src/App.tsx
@@ -90,7 +90,7 @@ function App() {
       {isBracket === true && 
         <Modal func={() => {setIsBracket(false)}}>
           <div className='embedt'>
-            <iframe className='register' src="https://start.gg/tournament/pfl-ranking-5-road-to-evo-2k23/register/embed"></iframe>
+            <iframe className='register' src="https://www.start.gg/tournament/pfl-ranking-6-road-to-evo-2k23/register/embed"></iframe>
           </div>
         </Modal>
       }
